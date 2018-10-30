cc1ad9683823b06de8b87ef2c9914bdf813a4f6f
NickeGilbert <nicklas.gilbertson@gmail.com>
2018-10-30 14:49:49 +0100

    Initial commit

---
+++ app/src/main/java/com/myprojects/nicklasgilbertsson/meditation/FirstActivity.java
@@ -0,0 +1,89 @@
+package com.myprojects.nicklasgilbertsson.meditation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+        Button myButton = (Button) findViewById(R.id.button1);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ArrayList<Class> activityList = new ArrayList<>();
+                Bundle extras = getIntent().getExtras();
+                activityList = (ArrayList<Class>) extras.get("ACTIVITY_LIST");
+
+                if(activityList.size() == 0) {
+                    // Do something when after all activities have been opened
+               //     doSomeAction();
+                } else {
+                    // Now, the random number is generated between 1 and however many
+                    // activities we have remaining
+                    Random generator = new Random();
+                    int number = generator.nextInt(activityList.size()) + 1;
+
+                    Class activity = null;
+
+                    // Here, we are checking to see what the output of the random was
+                    switch(number) {
+                        case 1:
+                            // We will open the first remaining activity of the list
+                            activity = activityList.get(0);
+                            // We will now remove that activity from the list
+                           // activityList.remove(0);
+                            break;
+                        case 2:
+                            // We will open the second remaining activity of the list
+                            activity = activityList.get(1);
+                           // activityList.remove(1);
+                            break;
+                        case 3:
+                            // We will open the third remaining activity of the list
+                            activity = activityList.get(2);
+                          //  activityList.remove(2);
+                            break;
+                        case 4:
+                            // We will open the fourth remaining activity of the list
+                            activity = activityList.get(3);
+                         //   activityList.remove(3);
+                            break;
+                        case 5:
+                            activity = activityList.get(4);
+                            break;
+                        case 6:
+                            activity = activityList.get(5);
+                            break;
+                        default:
+                            // We will open the fifth remaining activity of the list
+                            activity = activityList.get(6);
+                            //        activityList.remove(4);
+                            break;
+                    }
+
+                    // Note: in the above, we might not have 3 remaining activities, for example,
+                    // but it doesn't matter because that case wouldn't be called anyway,
+                    // as we have already decided that the number would be between 1 and the number of
+                    // activities left.
+
+
+                    // Starting the activity, and passing on the remaining number of activities
+                    // to the next one that is opened
+                    Intent intent = new Intent(getBaseContext(), activity);
+                    intent.putExtra("ACTIVITY_LIST", activityList);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/myprojects/nicklasgilbertsson/meditation/MainActivity.java
@@ -0,0 +1,76 @@
+package com.myprojects.nicklasgilbertsson.meditation;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button myButton = (Button) findViewById(R.id.MY_BUTTON);
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We are creating a list, which will store the activities that haven't been opened yet
+                ArrayList<Class> activityList = new ArrayList<>();
+                activityList.add(FirstActivity.class);
+                activityList.add(SecondActivity.class);
+                activityList.add(ThirdActivity.class);
+                activityList.add(FourthActivity.class);
+                activityList.add(FifthActivity.class);
+                activityList.add(SixthActivity.class);
+                activityList.add(SeventhActivity.class);
+
+                Random generator = new Random();
+                int number = generator.nextInt(7) + 1;
+
+                Class activity = null;
+
+                // Here, we are checking to see what the output of the random was
+                switch(number) {
+                    case 1:
+                        activity = FirstActivity.class;
+                        // We are adding the number of the activity to the list
+                  //      activityList.remove(FirstActivity.class);
+                        break;
+                    case 2:
+                        activity = SecondActivity.class;
+                   //     activityList.remove(SecondActivity.class);
+                        break;
+                    case 3:
+                        activity = ThirdActivity.class;
+                    //    activityList.remove(ThirdActivity.class);
+                        break;
+                    case 4:
+                        activity = FourthActivity.class;
+                    //    activityList.remove(FourthActivity.class);
+                        break;
+                    case 5:
+                        activity = FifthActivity.class;
+                        break;
+                    case 6:
+                        activity = SixthActivity.class;
+                        break;
+                    default:
+                        activity = SeventhActivity.class;
+                      //  activityList.remove(FifthActivity.class);
+                        break;
+                }
+                // We use intents to start activities
+                Intent intent = new Intent(getBaseContext(), activity);
+                // `intent.putExtra(...)` is used to pass on extra information to the next activity
+                intent.putExtra("ACTIVITY_LIST", activityList);
+                startActivity(intent);
+            }
+        });
+    }
+}
